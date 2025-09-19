eaf855f9c9f2a1275e376d49f13bbe5ae38410ea
Qurt <iskander.taniev3@gmail.com>
2025-09-19 09:59:25 -0400

    Initial commit

---
+++ 4sJ1/src/Glasses.java
@@ -0,0 +1,10 @@
+public class Glasses {
+    String color;
+
+    Glasses(string color){
+        this.color = color;
+    }
+    void display(){
+
+    }
+}
---
+++ 4sJ1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Glasses glasses = new Glasses(color:"Blue");
+        glasses.display();
+
+        Human h = new Human(name: "Jacque", age: 23, Heart: heart);
+    }
+}
